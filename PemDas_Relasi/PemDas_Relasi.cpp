d56817e65c8ca6e9e22e920acd2fed00aac9912f
ZahwaAnritia <anritia15@gmail.com>
2024-05-20 11:38:22 +0700

    menambahkan deklarasi global

--- PemDas_Relasi/PemDas_Relasi.cpp
+++ PemDas_Relasi/PemDas_Relasi.cpp
@@ -5,7 +5,6 @@ using namespace std;
 #include "jantung.h"
 
 
-
 int main()
 {
     
--- aggregation/aggregation.cpp
+++ aggregation/aggregation.cpp
@@ -1,5 +1,10 @@
-
 #include <iostream>
+#include <string>
+using namespace std;
+
+#include "anak.h"
+#include "ibu.h"
+
 
 int main()
 {
