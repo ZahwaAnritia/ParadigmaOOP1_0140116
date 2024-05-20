4378b9a2cfe83c412fb39aa582c8bc0b9211e7a8
ZahwaAnritia <anritia15@gmail.com>
2024-05-20 11:32:17 +0700

    menambahkan void ibu

--- aggregation/Ibu.h
+++ aggregation/Ibu.h
@@ -16,6 +16,11 @@ public:
 	~ibu() {
 		cout << " Ibu \" " << nama << "\" tidak ada\n";
 	}
+	void tambahAnak(anak*);
+	void cetakAnak();
 };
+void ibu::tambahAnak(anak* pAnak) {
+	daftar_anak.push_back(pAnak);
+}
 
 #endif
